aaa95f30c9a98022a570d8ae2797979977e77e5a
Bill Jones <misterunix@gmail.com>
2025-03-08 21:37:01 -0600

    chore: Update build script for debug and release binaries

--- build.sh
+++ build.sh
@@ -3,5 +3,12 @@ archs=(amd64 arm64 ppc64le ppc64 s390x 386)
 
 for arch in ${archs[@]}
 do
-	env GOOS=linux GOARCH=${arch} go build -o bin/writing-prompt_linux_${arch}
+	env GOOS=linux GOARCH=${arch} go build -o bin/writing-prompt-linux-debug-${arch}
 done
+
+
+for arch in ${archs[@]}
+do
+	env GOOS=linux GOARCH=${arch} go build -ldflags="-w -s" -o bin/writing-prompt-linux-release-${arch}
+done
+

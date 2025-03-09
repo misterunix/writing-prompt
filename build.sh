e19eead34225ebba5dd2bfdd92bc47949dec77f8
Bill Jones <misterunix@gmail.com>
2025-03-09 15:26:56 -0500

    chore: Remove unused binaries, update .gitignore, and implement title cleaning functionality Getting ready to add markov chains to the project. This commit removes the unused binaries from the project, updates the .gitignore file, and implements a function to clean the titles of the input data. The title cleaning function removes any non-alphanumeric characters from the titles and converts them to lowercase. This will help to improve the quality of the markov chains generated from the input data.

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 archs=(amd64 arm64)
 
-for arch in ${archs[@]}
-do
-	env GOOS=linux GOARCH=${arch} go build -o bin/writing-prompt-linux-debug-${arch}
-done
+#for arch in ${archs[@]}
+#do
+#	env GOOS=linux GOARCH=${arch} go build -o bin/writing-prompt-linux-debug-${arch}
+#done
 
 
 for arch in ${archs[@]}
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	_ "embed"
+	"flag"
 	"fmt"
 	"math/rand/v2"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -28,6 +30,15 @@ var Rplottwists string
 
 func main() {
 
+	var bookTitle string
+	flag.StringVar(&bookTitle, "t", "", "Book title to clean.")
+	flag.Parse()
+
+	if bookTitle != "" {
+		cleanTitle(bookTitle)
+		os.Exit(0)
+	}
+
 	characters := strings.Split(Rcharacters, "\n")
 	descriptions := strings.Split(Rdescriptions, "\n")
 	names := strings.Split(Rnames, "\n")
@@ -49,17 +60,9 @@ func main() {
 	action := actions[rand.IntN(acount)]
 	plottwist := plottwists[rand.IntN(pcount)]
 
-	//line := description + " " + character + " " + action + " " + plottwist + " " + setting
-	// fmt.Println("Generated line:")
-	// fmt.Println("Character: ", character)
-	// fmt.Println("Action: ", action)
-	// fmt.Println("Description: ", description)
-	// fmt.Println("Setting: ", setting)
-	// fmt.Println("Plottwist: ", plottwist)
-
-	save := fmt.Sprintf("%s %s, %s, %s, %s, %s\n", character, name, action, description, setting, plottwist)
+	save := fmt.Sprintf("|%s %s|%s|%s|%s|%s|\n", character, name, action, description, setting, plottwist)
 	// Save to file
-	f, err := os.OpenFile("slugs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile("slugs.md", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -70,4 +73,81 @@ func main() {
 
 	fmt.Println(save)
 
+}
+
+func cleanTitle(filename string) error {
+
+	var titles []string // tempory slice to hold titles
+
+	f, err := os.ReadFile(filename) // binary read file
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	lines := strings.Split(string(f), "\n") // split file into lines
+
+	// loop through lines and strip white space
+	for _, line := range lines {
+
+		striped := strings.TrimSpace(line)
+
+		// skip empty lines
+		if striped == "" {
+			continue
+		}
+
+		// skip single word titles
+		if len(striped) == 1 {
+			continue
+		}
+
+		paren := false
+		cc := 0
+		for {
+			cc++
+			fmt.Println(cc, striped)
+			if cc > 100 {
+				break
+			}
+			if strings.Contains(striped, "(") {
+				p1 := strings.Index(striped, "(")
+				p2 := strings.Index(striped, ")")
+				if p1 == -1 || p2 == -1 {
+					break
+				}
+				striped = striped[:p1-1] + striped[p2+1:]
+				paren = true
+			} else {
+				paren = false
+			}
+			if !paren {
+				break
+			}
+		}
+
+		// split title and author
+		t := strings.Split(striped, " by ")
+
+		// add title to slice
+		titles = append(titles, t[0])
+	}
+
+	sort.StringSlice(titles).Sort() // sort titles
+
+	l := len(titles) // get length of titles
+
+	for i := 0; i < l-1; i++ {
+		for j := i + 1; j < l; j++ {
+			if titles[i] == titles[j] {
+				titles = append(titles[:j], titles[j+1:]...)
+				j--
+				l--
+			}
+		}
+	}
+
+	// save to file
+	os.WriteFile("cleaned.txt", []byte(strings.Join(titles, "\n")), 0644)
+	return nil
 }

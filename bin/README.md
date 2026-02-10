76ec59441d46e73c7ccd3403a904c10310950b00
Bill Jones <misterunix@gmail.com>
2026-02-10 13:42:00 -0600

    refactor: Update README and build script for improved binary handling and add embedded data files

--- bin/README.md
+++ bin/README.md
@@ -1,1 +1,9 @@
-# binaries go here
+# binaries live here
+
+## Current platforms and filenames
+
+- writing-prompt-linux-amd64
+- writing-prompt-linux-arm64
+- writing-prompt-darwin-amd64
+- writing-prompt-darwin-arm64
+- writing-prompt-windows-amd64.exe
---
+++ embeds.go
@@ -0,0 +1,91 @@
+package main
+
+import _ "embed"
+
+//go:embed data/0/0-a.gz
+var R0a []byte
+
+//go:embed data/0/0-c.gz
+var R0c []byte
+
+//go:embed data/0/0-d.gz
+var R0d []byte
+
+//go:embed data/0/0-n.gz
+var R0n []byte
+
+//go:embed data/0/0-p.gz
+var R0p []byte
+
+//go:embed data/0/0-s.gz
+var R0s []byte
+
+//
+//
+//
+
+//go:embed data/1/1-a.gz
+var R1a []byte
+
+//go:embed data/1/1-c.gz
+var R1c []byte
+
+//go:embed data/1/1-d.gz
+var R1d []byte
+
+//go:embed data/1/1-n.gz
+var R1n []byte
+
+//go:embed data/1/1-p.gz
+var R1p []byte
+
+//go:embed data/1/1-s.gz
+var R1s []byte
+
+//
+//
+//
+
+//go:embed data/2/2-a.gz
+var R2a []byte
+
+//go:embed data/2/2-c.gz
+var R2c []byte
+
+//go:embed data/2/2-d.gz
+var R2d []byte
+
+//go:embed data/2/2-n.gz
+var R2n []byte
+
+//go:embed data/2/2-p.gz
+var R2p []byte
+
+//go:embed data/2/2-s.gz
+var R2s []byte
+
+//
+//
+//
+
+//go:embed data/3/3-a.gz
+var R3a []byte
+
+//go:embed data/3/3-c.gz
+var R3c []byte
+
+//go:embed data/3/3-d.gz
+var R3d []byte
+
+//go:embed data/3/3-n.gz
+var R3n []byte
+
+//go:embed data/3/3-p.gz
+var R3p []byte
+
+//go:embed data/3/3-s.gz
+var R3s []byte
+
+//
+//
+//

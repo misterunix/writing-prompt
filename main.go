20fd219a870b23559f1460d78a238a32ee743578
Bill Jones <misterunix@gmail.com>
2026-02-10 10:09:25 -0600

    refactor: Update README and main.go for improved data handling and remove unused build steps

--- main.go
+++ main.go
@@ -9,9 +9,7 @@ import (
 	"io"
 	"math/rand/v2"
 	"os"
-	"path"
 	"sort"
-	"strconv"
 	"strings"
 )
 
@@ -88,27 +86,27 @@ type markov struct {
 
 var port int = 5544
 var logging bool = false
+var mode int
 
 var (
-	characters   []string
-	names        []string
-	actions      []string
-	descriptions []string
-	settings     []string
-	plottwists   []string
+	characters   [3][]string
+	names        [3][]string
+	actions      [3][]string
+	descriptions [3][]string
+	settings     [3][]string
+	plottwists   [3][]string
 )
 
 func main() {
 
-	workingDir, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	// workingDir, err := os.Getwd()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
 
 	var countWanted int // number of slugs to generate
 	var bookTitle string
-	var mode int
 
 	// parse command line arguments
 	flag.IntVar(&countWanted, "n", 1, "Number of slugs to generate.")
@@ -125,74 +123,162 @@ func main() {
 		os.Exit(0)
 	}
 
-	dataDirBase := path.Join(workingDir, "data")
-	dataFolder := strconv.Itoa(mode)
-	dataDir := path.Join(dataDirBase, dataFolder)
+	//
+	// 0
+	//
+	dcd, err := GzipDecompress(R0c)
+	if err != nil {
+		fmt.Println("Error decompressing R0c:", err)
+		os.Exit(1)
+	}
+	characters[0] = strings.Split(string(dcd), "\n")
 
-	// check if data directory exists
-	if _, err := os.Stat(dataDir); os.IsNotExist(err) {
-		fmt.Printf("Data directory '%s' does not exist.\n", dataDir)
+	dcd, err = GzipDecompress(R0n)
+	if err != nil {
+		fmt.Println("Error decompressing R0n:", err)
 		os.Exit(1)
 	}
+	names[0] = strings.Split(string(dcd), "\n")
 
-	actionsRaw, err := os.ReadFile(path.Join(dataDir, "actions.txt"))
+	dcd, err = GzipDecompress(R0a)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error decompressing R0a:", err)
 		os.Exit(1)
 	}
-	actions = strings.Split(string(actionsRaw), "\n")
-	charactersRaw, err := os.ReadFile(path.Join(dataDir, "characters.txt"))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	characters = strings.Split(string(charactersRaw), "\n")
-	descriptionsRaw, err := os.ReadFile(path.Join(dataDir, "descriptions.txt"))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	descriptions = strings.Split(string(descriptionsRaw), "\n")
-	namesRaw, err := os.ReadFile(path.Join(dataDir, "names.txt"))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	names = strings.Split(string(namesRaw), "\n")
-	plottwistsRaw, err := os.ReadFile(path.Join(dataDir, "plottwists.txt"))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	plottwists = strings.Split(string(plottwistsRaw), "\n")
-	settingsRaw, err := os.ReadFile(path.Join(dataDir, "settings.txt"))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	settings = strings.Split(string(settingsRaw), "\n")
+	actions[0] = strings.Split(string(dcd), "\n")
 
-	// get home directory
-	// homeDir, err := os.UserHomeDir()
+	dcd, err = GzipDecompress(R0d)
+	if err != nil {
+		fmt.Println("Error decompressing R0d:", err)
+		os.Exit(1)
+	}
+	descriptions[0] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R0s)
+	if err != nil {
+		fmt.Println("Error decompressing R0s:", err)
+		os.Exit(1)
+	}
+	settings[0] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R0p)
+	if err != nil {
+		fmt.Println("Error decompressing R0p:", err)
+		os.Exit(1)
+	}
+	plottwists[0] = strings.Split(string(dcd), "\n")
+
+	//
+	//
+	//
+
+	dcd, err = GzipDecompress(R1c)
+	if err != nil {
+		fmt.Println("Error decompressing R1c:", err)
+		os.Exit(1)
+	}
+	characters[1] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R1n)
+	if err != nil {
+		fmt.Println("Error decompressing R1n:", err)
+		os.Exit(1)
+	}
+	names[1] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R1a)
+	if err != nil {
+		fmt.Println("Error decompressing R1a:", err)
+		os.Exit(1)
+	}
+	actions[1] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R1d)
+	if err != nil {
+		fmt.Println("Error decompressing R1d:", err)
+		os.Exit(1)
+	}
+	descriptions[1] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R1s)
+	if err != nil {
+		fmt.Println("Error decompressing R1s:", err)
+		os.Exit(1)
+	}
+	settings[1] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R1p)
+	if err != nil {
+		fmt.Println("Error decompressing R1p:", err)
+		os.Exit(1)
+	}
+	plottwists[1] = strings.Split(string(dcd), "\n")
+
+	//
+	//
+	//
+
+	dcd, err = GzipDecompress(R2c)
+	if err != nil {
+		fmt.Println("Error decompressing R2c:", err)
+		os.Exit(1)
+	}
+	characters[2] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R2n)
+	if err != nil {
+		fmt.Println("Error decompressing R2n:", err)
+		os.Exit(1)
+	}
+	names[2] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R2a)
+	if err != nil {
+		fmt.Println("Error decompressing R2a:", err)
+		os.Exit(1)
+	}
+	actions[2] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R2d)
+	if err != nil {
+		fmt.Println("Error decompressing R2d:", err)
+		os.Exit(1)
+	}
+	descriptions[2] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R2s)
+	if err != nil {
+		fmt.Println("Error decompressing R2s:", err)
+		os.Exit(1)
+	}
+	settings[2] = strings.Split(string(dcd), "\n")
+
+	dcd, err = GzipDecompress(R2p)
+	if err != nil {
+		fmt.Println("Error decompressing R2p:", err)
+		os.Exit(1)
+	}
+	plottwists[2] = strings.Split(string(dcd), "\n")
+
+	//
+	//
+	//
+
+	// create data directory
+	// csvDir := path.Join(workingDir, "writing-prompts")
+	// err = os.MkdirAll(csvDir, os.ModePerm)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
+	// fmt.Print("Data directory: '", csvDir, "'\n")
+
+	// // create temp file in data directory
+	// f, err := os.CreateTemp(csvDir, "slugs-*.csv")
 	// if err != nil {
 	// 	fmt.Println(err)
 	// }
-
-	// create data directory
-	csvDir := path.Join(workingDir, "writing-prompts")
-	err = os.MkdirAll(csvDir, os.ModePerm)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fmt.Print("Data directory: '", csvDir, "'\n")
-
-	// create temp file in data directory
-	f, err := os.CreateTemp(csvDir, "slugs-*.csv")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer f.Close()
+	// defer f.Close()
 
 	// loop to generate slugs
 	for range countWanted {
@@ -212,10 +298,10 @@ func main() {
 			character, name, action, description, setting, plottwist)
 
 		// write slug to file
-		if _, err := f.WriteString(save); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		// if _, err := f.WriteString(save); err != nil {
+		// 	fmt.Println(err)
+		// 	os.Exit(1)
+		//}
 
 		// print slug to console
 		fmt.Printf("%s", save)
@@ -223,19 +309,19 @@ func main() {
 }
 
 func createSlug() (string, string, string, string, string, string) {
-	dcount := len(descriptions)
-	ccount := len(characters)
-	ncount := len(names)
-	scount := len(settings)
-	acount := len(actions)
-	pcount := len(plottwists)
+	dcount := len(descriptions[mode])
+	ccount := len(characters[mode])
+	ncount := len(names[mode])
+	scount := len(settings[mode])
+	acount := len(actions[mode])
+	pcount := len(plottwists[mode])
 
-	character := characters[rand.IntN(ccount)]
-	name := names[rand.IntN(ncount)]
-	setting := settings[rand.IntN(scount)]
-	action := actions[rand.IntN(acount)]
-	description := descriptions[rand.IntN(dcount)]
-	plottwist := plottwists[rand.IntN(pcount)]
+	character := characters[mode][rand.IntN(ccount)]
+	name := names[mode][rand.IntN(ncount)]
+	setting := settings[mode][rand.IntN(scount)]
+	action := actions[mode][rand.IntN(acount)]
+	description := descriptions[mode][rand.IntN(dcount)]
+	plottwist := plottwists[mode][rand.IntN(pcount)]
 	return character, name, action, description, setting, plottwist
 }
 
